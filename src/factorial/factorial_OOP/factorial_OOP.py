9b9039fd3f11b0ccd7b8a90f727bc2d1a78ea7fd
Giovanni202dos <palaciosgiovanniandres1999@gmail.com>
2023-03-29 16:03:21 -0300

    arreglando los metodos

--- src/factorial/factorial_OOP/factorial_OOP.py
+++ src/factorial/factorial_OOP/factorial_OOP.py
@@ -1,37 +1,24 @@
 import sys
 
 class Factorial:    #clase Factorial
-    def __init__(self, min, max):
-        pass
+    def __init__(self, min, max):   #constructor
+        self.run(min,max)
 
-    def run(self,num):  #Metodo run (facotorial de un solo numero)
-        if num < 0: 
-            print("Factorial de un número negativo no existe")
+    def run(self,min,max):  #Metodo run
+        for num in range(min,max+1):
+            numero=num
+            if num < 0: 
+                print("Factorial de un número negativo no existe")
 
-        elif num == 0: 
-            return 1
-            
-        else: 
-            fact = 1
-            while(num > 1): 
-                fact *= num 
-                num -= 1
-            return fact 
-    def factorial_en_range(self,min,max):   #Metodo para calcular el facotorial en un rango
-        for i in range(min , max+1):
-            num = max
-            #print("num:", num)
-            #print("max-antes:", max)
-            fact = 1
-            #print("fact:", fact)
-            while(num >= min and num!=0): 
-                fact *= num 
-                #print('fff ', fact)
-                num -= 1
-                #print('num fff ', num)
-            print("Factorial ",max,"! es ", fact) 
-            max -=1
-            #print("max-despues:", max)
+            elif num == 0: 
+                print("Factorial ",num,"! es ", 1)
+                
+            else: 
+                fact = 1
+                while(num > 1): 
+                    fact *= num 
+                    num -= 1
+                print("Factorial ",numero,"! es ", fact)
             
 
 
@@ -47,6 +34,5 @@ else:
         print("Debe ingresar un rango adecuado!")
         sys.exit()
     else:
-        factoriall = Factorial() #creamos una instancia de la clase
-        factoriall.factorial_en_range(desde , hasta)
+        factoriall = Factorial(desde , hasta) #creamos una instancia de la clase
 
