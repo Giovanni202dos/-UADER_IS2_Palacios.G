bdb4f30b1f469cc9846dcc99d31db5a414f1679e
Giovanni202dos <palaciosgiovanniandres1999@gmail.com>
2023-03-29 21:46:23 -0300

    eliminando lineas inecesarias

--- src/numero_de_Collatz/collatz.py
+++ src/numero_de_Collatz/collatz.py
@@ -3,28 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def collatz(num,contador=0):
-    #print('principal')
-    #print('num: ',num)
-    #print('contador: ',contador)
-    #input()
+
     if num%2==0:    #si es par
-        #print('entro al par')
         num=num/2
         contador+=1
         contador=collatz(num,contador) 
-        #print('fff: ',contador) 
-        #print("nummm: ",num) 
     elif(num!=1):   #si es impar y diferente de 1(ya q si es uno se termina todo)
-        #print('entro al impar')
         num=(3*num)+1
         contador+=1
-        #print('num-desp: ',num)
-        #print('contador-desp: ',contador)
         contador=collatz(num,contador)
-        #print('fffaaa: ',contador) 
     return contador
 
-#sys.argv.append('2-3')
+
 if len(sys.argv) == 1:  #cambie por 1(habia un cero) si hay un elemento guardado es porque no ingreso el numero
    print("Debe ingresar un rango!")
    sys.exit()
